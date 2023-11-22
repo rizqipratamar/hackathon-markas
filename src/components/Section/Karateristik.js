579f6dea89ffc0beca4ff21d057ef622ebfa6a13
Dimas Ardana <dimas.ardana@mceasy.co.id>
2023-11-23 02:11:13 +0700

    dana: slicing home page, phase one

--- src/components/Section/Karateristik.js
+++ src/components/Section/Karateristik.js
@@ -1,56 +1,58 @@
 const KarateristikSection = () => {
   return (
     <div className="text-center">
-      <div className="text-primary text-[28px]">KARAKTERISTIK</div>
-      <div className="text-[48px] mt-5 font-bold">10 KARAKTER MARKAS</div>
-      <div className="text-[20px] mt-[35px] mb-20">
-        Sejak diinisiasi di tahun 2016, semangat #1000StartupDigital adalah
-        untuk berkolaborasi <br/>membuat ekosistem startup Indonesia menjadi inklusif
-        dan kolaboratif.
+      <div className="text-primary text-[28px] font-semibold mt-[165px]">
+        KARAKTERISTIK
       </div>
-      <div className="my-[83px] grid grid-cols-1 gap-3">
+      <div className="text-5xl font-extrabold mt-5">10 KARAKTER MARKAS</div>
+      <div className="text-xl font-light mt-[35px] mb-20">
+        Sejak diinisiasi di tahun 2016, semangat #1000StartupDigital adalah
+        untuk berkolaborasi <br />
+        membuat ekosistem startup Indonesia menjadi inklusif dan kolaboratif.
+      </div>
+      <div className="my-[83px] grid grid-cols-1 gap-3 max-w-6xl flex">
         <div className="flex gap-5 mx-auto">
-            <div className="border-2 px-[40px] text-[30px] py-5 border-black rounded-full">
+          <div className="border-2 text-3xl py-5 px-12 border-black rounded-full">
             🚀 VISIONARY
-            </div>
-            <div className="border-2 px-[40px] text-[30px] py-5 border-black rounded-full">
-            🚀 VISIONARY
-            </div>
-            <div className="border-2 px-[40px] text-[30px] py-5 border-black rounded-full">
-            🚀 VISIONARY
-            </div>
+          </div>
+          <div className="border-2 text-3xl py-5 px-12 border-black rounded-full">
+            👟️ EXTRAMILE
+          </div>
+          <div className="border-2 text-3xl py-5 px-12 border-black rounded-full">
+            🧠 STRATEGIC
+          </div>
         </div>
         <div className="flex gap-5 mx-auto">
-            <div className="border-2 px-[40px] text-[30px] py-5 border-black rounded-full">
-            🚀 VISIONARY
-            </div>
-            <div className="border-2 px-[40px] text-[30px] py-5 border-black rounded-full">
-            🚀 VISIONARY
-            </div>
-            <div className="border-2 px-[40px] text-[30px] py-5 border-black rounded-full">
-            🚀 VISIONARY
-            </div>
-            <div className="border-2 px-[40px] text-[30px] py-5 border-black rounded-full">
-            🚀 VISIONARY
-            </div>
+          <div className="border-2 text-3xl py-5 px-12 border-black rounded-full">
+            🏆 GRIT
+          </div>
+          <div className="border-2 text-3xl py-5 px-12 border-black rounded-full">
+            ⚖ INTEGRITY
+          </div>
+          <div className="border-2 text-3xl py-5 px-12 border-black rounded-full">
+            🎯 FOCUSED
+          </div>
+          <div className="border-2 text-3xl py-5 px-12 border-black rounded-full">
+            🚅 AGILE
+          </div>
         </div>
         <div className="flex gap-5 mx-auto">
-            <div className="border-2 px-[40px] text-[30px] py-5 border-black rounded-full">
-            🚀 VISIONARY
-            </div>
-            <div className="border-2 px-[40px] text-[30px] py-5 border-black rounded-full">
-            🚀 VISIONARY
-            </div>
-            <div className="border-2 px-[40px] text-[30px] py-5 border-black rounded-full">
-            🚀 VISIONARY
-            </div>
+          <div className="border-2 text-3xl py-5 px-12 border-black rounded-full">
+            🔥 PASSION
+          </div>
+          <div className="border-2 text-3xl py-5 px-12 border-black rounded-full">
+            📚 RESOURCEFUL
+          </div>
+          <div className="border-2 text-3xl py-5 px-12 border-black rounded-full">
+            🎨 CREATIVE
+          </div>
         </div>
       </div>
       <div className="w-full flex">
-          <button className="btn btn-lg btn-primary mx-auto text-[28px] rounded-full ">
-            Cari tahu lebih
-          </button>
-        </div>
+        <button className="btn btn-lg btn-primary mx-auto text-[28px] rounded-full ">
+          Cari tahu lebih
+        </button>
+      </div>
     </div>
   );
 };
